869e8dfb7787e57400c346aaff9ff6f6161df888
Mladen Petrov <mladen.petrov88@gmail.com>
2019-10-04 10:06:29 +0300

    Add files via upload

---
+++ TownInfo.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class TownInfo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.next();
+        int population = scanner.nextInt();
+        int area = scanner.nextInt();
+
+        System.out.printf("Town %s has population of %d and area %d square km.", name, population, area);
+    }
+}
